476747c21083b66ea23a9bb56db53226253d91ea
Arthur Lima <artglima.dev@gmail.com>
2024-12-12 15:20:46 -0300

    feat: disabling new input

--- src/app.tsx
+++ src/app.tsx
@@ -19,12 +19,12 @@ export function App() {
           <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
             <div className="flex items-center gap-2 flex-1">
               <MapPin className="size-5 text-zinc-400"/>
-              <input type="text" placeholder="Para onde você vai" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
             </div>
             
             <div className="flex items-center gap-2">
               <Calendar className="size-5 text-zinc-400"/>
-              <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+              <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
             </div>
 
             <div className="w-px h-6 bg-zinc-800"/>
